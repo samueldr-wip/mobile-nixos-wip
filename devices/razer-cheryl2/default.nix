910178b304751507e36118f4533a1039909b706e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-09-21 16:35:18 -0400

    WIP with razer's config

--- devices/razer-cheryl2/default.nix
+++ devices/razer-cheryl2/default.nix
@@ -36,27 +36,46 @@
   mobile.system.vendor.partition = "/dev/disk/by-partlabel/vendor_a";
 
   boot.kernelParams = [
-    # From TWRP
-    "console=ttyMSM0,115200n8"
-    "earlycon=msm_geni_serial,0xA84000"
-    "androidboot.hardware=qcom"
-    "androidboot.console=ttyMSM0"
-    "video=vfb:640x400,bpp=32,memsize=3072000"
-    "msm_rtb.filter=0x237"
-    "ehci-hcd.park=3"
-    "lpm_levels.sleep_disabled=1"
-    "service_locator.enable=1"
-    "swiotlb=2048"
-    "androidboot.configfs=true"
-    #"firmware_class.path=/vendor/firmware_mnt/image"
-    "loop.max_part=7"
-    "androidboot.usbcontroller=a600000.dwc3"
-    "buildvariant=user"
-    "printk.devkmsg=on"
+    "hellorazer=hi_1"
+"console=ttyMSM0,115200n8"
+"earlycon=msm_geni_serial,0xA84000"
+"androidboot.hardware=qcom"
+"androidboot.console=ttyMSM0"
+"video=vfb:640x400,bpp=32,memsize=3072000"
+"msm_rtb.filter=0x237"
+"ehci-hcd.park=3"
+"lpm_levels.sleep_disabled=1"
+"service_locator.enable=1"
+"swiotlb=2048"
+"androidboot.configfs=true"
+"firmware_class.path=/vendor/firmware_mnt/image"
+"loop.max_part=7"
+"androidboot.usbcontroller=a600000.dwc3"
+"buildvariant=user"
 
-
-    # Using `quiet` fixes early framebuffer init, for stage-1
-    "quiet"
+##     # From TWRP
+## ##    "console=ttyMSM0,115200n8"
+## ##    "earlycon=msm_geni_serial,0xA84000"
+##     "hellorazer=3"
+## ##    "androidboot.hardware=qcom"
+## ##    "androidboot.console=ttyMSM0"
+##     "video=vfb:640x400,bpp=32,memsize=3072000"
+## ##    "msm_rtb.filter=0x237"
+## ##    "ehci-hcd.park=3"
+## ##    "lpm_levels.sleep_disabled=1"
+## ##    "service_locator.enable=1"
+## ##    "swiotlb=2048"
+## ##    "androidboot.configfs=true"
+## ##    #"firmware_class.path=/vendor/firmware_mnt/image"
+## ##    "loop.max_part=7"
+## ##    "androidboot.usbcontroller=a600000.dwc3"
+## ##    "buildvariant=user"
+##     "printk.devkmsg=on"
+##     "console=tty1" # ???
+## 
+## 
+##     # Using `quiet` fixes early framebuffer init, for stage-1
+##     #"quiet"
   ];
 
   mobile.system.type = "android";
@@ -73,3 +92,33 @@
     # adb = "ffs.usb0";
   };
 }
+
+/*
+
+NOTES:
+
+Next try:
+
+XX https://github.com/arter97/android_kernel_razer_sdm845/commit/29a968fb09d856c986d7b72bb17d6f3562df28f4
+XX 
+XX Confirm it's the same kernel as the r12 binary
+XX 
+XX https://arter97.com/browse/aura/
+XX https://arter97.com/browse/aura/kernel/r12/arter97-kernel-r12-20191028.img
+XX 
+XX First *just* build it with the config from arter as-is, and boot TWRP with it
+XX (by skipping initramfs)
+XX 
+XX THEN, add the framebuffer emulation options
+XX 
+XX THEN ????
+
+Can't get anything working that way...
+
+Next steps:
+
+Go directly from the source code release from Razer
+
+Extract **their** kernel configuration from their boot image
+
+*/
