16c6668bc2f21c64bbb339ed6536f36fe7356c5a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-13 17:06:41 -0400

    mobile-nixos.gui-assets: Init

---
+++ overlay/mobile-nixos/gui-assets/default.nix
@@ -0,0 +1,24 @@
+{ runCommandNoCC
+, overpass
+, roboto
+, font-awesome_4
+}:
+
+let
+  artwork = ../../../artwork;
+in
+runCommandNoCC "gui-assets" { } ''
+mkdir -p $out
+cp ${artwork + "/app-background.svg"} $out/app-background.svg
+cp ${artwork + "/logo/logo.white.svg"} $out/logo.svg
+mkdir -p $out/fonts
+cp -t $out/fonts \
+  ${roboto}/share/fonts/truetype/Roboto-Regular.ttf \
+  ${overpass}/share/fonts/opentype/overpass-bold.otf \
+  ${font-awesome_4}/share/fonts/opentype/FontAwesome.otf
+
+(
+  cd $out/fonts
+  ln -s FontAwesome.otf fallback.ttf
+)
+''
