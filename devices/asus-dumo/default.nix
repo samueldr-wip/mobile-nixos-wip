52fd70a62fee91578e4934f575198d460b04cb13
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-03 18:07:34 -0400

    asus-dumo: Refactor for using provenance

--- devices/asus-dumo/default.nix
+++ devices/asus-dumo/default.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [
+    ./mainline.nix
+    ./vendor.nix
+  ];
+
   mobile.device.name = "asus-dumo";
   mobile.device.identity = {
     name = "Chromebook Tablet CT100PA";
@@ -13,10 +18,6 @@
     screen = {
       width = 1536; height = 2048;
     };
-  };
-
-  mobile.boot.stage-1 = {
-    kernel.package = pkgs.callPackage ./kernel {};
   };
 
   mobile.system.depthcharge.kpart = {
@@ -35,47 +36,9 @@
 
   mobile.system.type = "depthcharge";
 
-  mobile.device.firmware = pkgs.callPackage ./firmware {};
-  mobile.boot.stage-1.firmware = [
-    config.mobile.device.firmware
+  # List of valid provenances
+  mobile.boot.stage-1.kernel.availableProvenances = [
+    "mainline"
+    "vendor"
   ];
-
-  # The controller is hidden from the OS unless started using the "android"
-  # launch option in the weird UEFI GUI chooser.
-  mobile.usb.mode = "gadgetfs";
-
-  # Commonly re-used values, Nexus 4 (debug)
-  # (These identifiers have well-known default udev rules.)
-  mobile.usb.idVendor = "18d1";
-  mobile.usb.idProduct = "d002";
-
-  # Mainline gadgetfs functions
-  mobile.usb.gadgetfs.functions = {
-    rndis = "rndis.usb0";
-    mass_storage = "mass_storage.0";
-    adb = "ffs.adb";
-  };
-
-  mobile.boot.stage-1.bootConfig = {
-    # Used by target-disk-mode to share the internal drive
-    storage.internal = "/dev/disk/by-path/platform-fe330000.mmc";
-  };
-
-  mobile.boot.stage-1.tasks = [
-    ./fixup_sdhci_arasan_task.rb
-    ./usb_role_switch_task.rb
-  ];
-
-  # This makes use of "atomic mode setting".
-  # This is needed for proper panel power management.
-  # *sigh*
-  services.xserver.deviceSection = ''
-    Option "Atomic" "True"
-  '';
-
-  # `mem` suspend doesn't work.
-  # (vendor chromeos-4.4 *and* mainline)
-  systemd.sleep.extraConfig = ''
-    SuspendState=freeze
-  '';
 }
