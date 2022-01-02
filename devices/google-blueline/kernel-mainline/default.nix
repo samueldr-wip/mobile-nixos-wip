658979ce0766300b48f77d63be819cc7e9f452a2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-01 20:40:46 -0500

    google-blueline: rebase kernel on sdm845-5.16
    *without* the touch panel enablement.

--- devices/google-blueline/kernel-mainline/default.nix
+++ devices/google-blueline/kernel-mainline/default.nix
@@ -1,41 +1,29 @@
-{
-  mobile-nixos
-, fetchurl
-, fetchpatch
+{ mobile-nixos
+, fetchFromGitLab
 , ...
 }:
 
-let
-  major = "5.15";
-  minor = "0";
-  rc = "-rc4";
-  downloadVersion = major;
-in
 mobile-nixos.kernel-builder rec {
-  version = "${major}.${minor}${rc}";
+  version = "5.16.0-rc5";
+  modDirVersion = "5.16.0-rc5";
   configfile = ./config.aarch64;
-
-  src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-${major}${rc}.tar.gz";
-    sha256 = "0n5asfajskzzhmx316xjb57qzvshz4s57lrmmsic0rw19w705hxq";
+  src = fetchFromGitLab {
+    owner = "sdm845-mainline";
+    repo = "linux";
+    rev = "9ed86f14d0718d81a55708abe5355fd2a8a09b8d";
+    sha256 = "sha256-/eBwyhFmBQ0yvbUQ3gv5lNTW7OE0SrwZdy9wVz6lOSA=";
   };
 
   patches = [
     ./0001-HACK-Add-back-TEXT_OFFSET-in-the-built-image.patch
 
-    # http://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/dsc_v2
-    # @ 7b8fcc2fcf1792a035ef35715a7db133a8d22635 
-    ./0001-Linaro-blueline-dsc-v2-wip.patch
+    # http://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/dsc_v3
+    # @ d5fe7f95e41a70eeb000498afd5aed48f9bf96ff
+    ./0001-Linaro-blueline-dsc-v3-wip.patch
 
     # https://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/gpi_i2c_touch
     # @ 1b3424a2994b82ac38fdf16e2136e7811548fea4
-    ./0002-Linaro-blueline-touch.patch
-
-    ./0001-dts-blueline-Configure-device-specific-firmware-path.patch
-
-    # Fixes regression with SDM845 devices on advice.
-    # A panic during `icc_init`.
-    ./0001-Revert-usb-dwc3-dwc3-qcom-Enable-tx-fifo-resize-prop.patch
+    # ./0002-Linaro-blueline-touch.patch
   ];
 
   # TODO: generic mainline build; append per-device...
@@ -46,7 +34,6 @@ mobile-nixos.kernel-builder rec {
       $buildRoot/arch/arm64/boot/Image.${isCompressed} \
       $out/
     )
-
     echo ':: Appending DTB'
     (PS4=" $ "; set -x
     cat \
