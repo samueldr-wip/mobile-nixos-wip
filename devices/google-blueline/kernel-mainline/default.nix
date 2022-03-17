cfc6b0b431c8542faae89afa5252d2ea1563842b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-16 22:10:55 -0400

    google-blueline: WIP kernel local copy of gsi7-pixel

--- devices/google-blueline/kernel-mainline/default.nix
+++ devices/google-blueline/kernel-mainline/default.nix
@@ -1,29 +1,24 @@
 { mobile-nixos
-, fetchFromGitLab
+, fetchFromGitHub
 , ...
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "5.16.0-rc5";
-  modDirVersion = "5.16.0-rc5";
+  version = "5.17.0-rc1";
   configfile = ./config.aarch64;
-  src = fetchFromGitLab {
-    owner = "sdm845-mainline";
+
+  # Exact copy of:
+  #  - https://git.linaro.org/people/vinod.koul/kernel.git/log/?h=topic/gsi7-pixel
+  #  - https://git.linaro.org/people/vinod.koul/kernel.git/commit/?h=topic/gsi7-pixel&id=d5ca4c5de8b28496ad565c91e974d8b2448bc80b
+  src = fetchFromGitHub {
+    owner = "samueldr";
     repo = "linux";
-    rev = "9ed86f14d0718d81a55708abe5355fd2a8a09b8d";
-    sha256 = "sha256-/eBwyhFmBQ0yvbUQ3gv5lNTW7OE0SrwZdy9wVz6lOSA=";
+    rev = "d5ca4c5de8b28496ad565c91e974d8b2448bc80b";
+    hash = "sha256-f8uoOV1+HYGZeTYiM48ydHtikbiAqsjURJvH1smo16o=";
   };
 
   patches = [
     ./0001-HACK-Add-back-TEXT_OFFSET-in-the-built-image.patch
-
-    # http://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/dsc_v3
-    # @ d5fe7f95e41a70eeb000498afd5aed48f9bf96ff
-    ./0001-Linaro-blueline-dsc-v3-wip.patch
-
-    # https://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/gpi_i2c_touch
-    # @ 1b3424a2994b82ac38fdf16e2136e7811548fea4
-    # ./0002-Linaro-blueline-touch.patch
   ];
 
   # TODO: generic mainline build; append per-device...
