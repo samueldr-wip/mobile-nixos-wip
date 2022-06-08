6e6a9f72b579db6518dccc07600df81aa3436d8b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-06-07 20:15:52 -0400

    nokia-argon: Add device

---
+++ devices/nokia-argon/default.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+
+{
+  mobile.device.name = "nokia-argon";
+  mobile.device.identity = {
+    name = "8110 4G";
+    manufacturer = "Nokia";
+  };
+
+  mobile.hardware = {
+    soc = "qualcomm-msm8905";
+    ram = 512;
+    screen = {
+      width = 240; height = 320;
+    };
+  };
+
+  mobile.boot.stage-1 = {
+    kernel.package = pkgs.callPackage ./kernel { };
+  };
+
+  mobile.system.android.bootimg = {
+    flash = {
+      offset_base = "0x80000000";
+      offset_kernel = "0x00008000";
+      offset_ramdisk = "0x01000000";
+      offset_second = "0x00f00000";
+      offset_tags = "0x00000100";
+      pagesize = "2048";
+    };
+  };
+
+  mobile.usb.mode = "android_usb";
+  # ID used in fastboot... not actually "correct"
+  mobile.usb.idVendor = "18d1";
+  mobile.usb.idProduct = "d00d";
+
+  mobile.system.type = "android";
+
+  mobile.boot.stage-1.compression = lib.mkDefault "xz";
+
+  mobile.kernel.structuredConfig = [
+    (helpers: with helpers; {
+      # With the vendor kernel, setting to =n fails the build
+      FW_LOADER_USER_HELPER = lib.mkForce yes;
+    })
+  ];
+}
