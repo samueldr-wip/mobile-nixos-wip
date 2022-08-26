51b9b69de613a681ff546d0498eeb906a05511ad
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-08-26 00:33:10 -0400

    kernel-builder: Apply randstruct only when needed

--- overlay/mobile-nixos/kernel/builder.nix
+++ overlay/mobile-nixos/kernel/builder.nix
@@ -221,7 +221,7 @@ stdenv.mkDerivation (inputArgs // {
   patches =
     map (p: p.patch) kernelPatches
     # Required for deterministic builds along with some postPatch magic.
-    ++ optional (lib.versionAtLeast version "4.13") (nixosKernelPath + "/randstruct-provide-seed.patch")
+    ++ optional ((lib.versionAtLeast version "4.13" && lib.versionOlder version "4.19")) (nixosKernelPath + "/randstruct-provide-seed.patch")
     ++ patches
   ;
 
