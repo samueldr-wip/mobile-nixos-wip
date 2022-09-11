f7da58d1ee19739a058c76e84e20b7fa9fdb41d9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-11 17:56:20 -0400

    XXX doesn't work

--- devices/google-blueline/kernel-mainline/default.nix
+++ devices/google-blueline/kernel-mainline/default.nix
@@ -5,20 +5,21 @@
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "5.19.0";
+  version = "5.19.7";
   configfile = ./config.aarch64;
 
-  src = fetchFromGitLab {
-    owner = "sdm845-mainline";
-    repo = "linux";
-    rev = "20795113b74df511fd599d3867a6bd1a5b0f9c48"; # XXX caleb/pixel3-bringup-5.19 DO NOT SHIP
-    hash = "sha256-rPvDe+xgClToIvKKbhC1y/fxeZnD+carx8zYy5KdsJ0=";
-  };
+  src = builtins.fetchGit /Users/samuel/tmp/linux/sdm845-mainline;
+  #src = fetchFromGitLab {
+  #  owner = "sdm845-mainline";
+  #  repo = "linux";
+  #  rev = "20795113b74df511fd599d3867a6bd1a5b0f9c48"; # XXX caleb/pixel3-bringup-5.19 DO NOT SHIP
+  #  hash = "sha256-rPvDe+xgClToIvKKbhC1y/fxeZnD+carx8zYy5KdsJ0=";
+  #};
 
   patches = [
     # Present in sd845-mainline WIP bringup branch already
     # ./0001-HACK-Add-back-TEXT_OFFSET-in-the-built-image.patch
-    ./0001-touchscreen-focaltech_fts-Add-missing-include.patch
+    #./0001-touchscreen-focaltech_fts-Add-missing-include.patch
   ];
 
   # TODO: generic mainline build; append per-device...
