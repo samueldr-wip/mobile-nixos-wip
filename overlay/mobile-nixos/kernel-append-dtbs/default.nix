2c25041e51429b4514bae14c9353cfbb1bb9a13a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-14 21:56:19 -0400

    mobile-nixos.kernel-append-dtbs: Forward override and version
    The override method shown here is a bit odd, but allows forwarding the
    arguments to the current call to an "overriden" kernel.
    
    In other words, we get the semantics of `override` on a kernel, which is
    kinda hard to get correct, for free. Though we re-apply the wrap
    afterward.
    
    This does not incurr an additional build to the kernel, as long as the
    `override` call wouldn't otherwise.

--- overlay/mobile-nixos/kernel-append-dtbs/default.nix
+++ overlay/mobile-nixos/kernel-append-dtbs/default.nix
@@ -4,29 +4,35 @@ let
   inherit (lib)
     escapeShellArgs
   ;
+
+  appendDTBs =
+    { kernel
+    /** List of dtb files to append; relative paths are relative to kernel derivation */
+    , dtbs
+    }:
+
+    runCommandNoCC "${kernel.name}-with-dtbs" {
+      passthru = kernel.passthru // {
+        inherit (kernel) version;
+        override = args: appendDTBs { kernel = kernel.override args; inherit dtbs; };
+      };
+    } ''
+      PS4=" $ "
+      mkdir $out
+      (
+      cd $out
+      for f in ${kernel}/*; do
+        ln -s "$f"
+      done
+      )
+      (
+      set -x
+      # cd so relative paths work implicitly.
+      cd ${kernel}
+      rm $out/${kernel.file}
+      cat ${kernel.file} ${escapeShellArgs dtbs} > $out/${kernel.file}
+      )
+    ''
+  ;
 in
-
-{ kernel
-/** List of dtb files to append; relative paths are relative to kernel derivation */
-, dtbs
-}:
-
-runCommandNoCC "${kernel.name}-with-dtbs" {
-  passthru = kernel.passthru;
-} ''
-  PS4=" $ "
-  mkdir $out
-  (
-  cd $out
-  for f in ${kernel}/*; do
-    ln -s "$f"
-  done
-  )
-  (
-  set -x
-  # cd so relative paths work implicitly.
-  cd ${kernel}
-  rm $out/${kernel.file}
-  cat ${kernel.file} ${escapeShellArgs dtbs} > $out/${kernel.file}
-  )
-''
+  appendDTBs
