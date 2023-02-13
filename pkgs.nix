38a4b30a730157282565960c01bf936249ed893c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-02-12 19:59:29 -0500

    XXX pkgs.nix update

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,6 @@
 let
-  sha256 = "sha256:1fhv0lfj7khfr0fvwbpay3vq3v7br86qq01yyl0qxls8nsq08y0c";
-  rev = "872fceeed60ae6b7766cc0a4cd5bf5901b9098ec";
+  sha256 = "sha256:1aznlrpins0j5cjmwzizcqiy38apkpv0hq5v5fb0d7r0vfzr2kn5";
+  rev = "5ed481943351e9fd354aeb557679624224de38d5";
 in
 builtins.trace "(Using pinned Nixpkgs at ${rev})"
 import (fetchTarball {
