6d8cde6d84d3915a011cfc84e2ffabf5d2033589
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-06 18:05:13 -0400

    pine64-pinenote: Add orientation patch to the kernel
    Before upstreaming, it will be important to entirely understand why the
    default is to reflect the panel.
    
    Anyway, what this does is to give the correct hints such that DRM-using
    apps can render in the correct orientation.
    
    This is needed for the Mobile NixOS stage-1 user interface, or else it's
    awkwardly set in the wrong orientation.

--- devices/pine64-pinenote/kernel/default.nix
+++ devices/pine64-pinenote/kernel/default.nix
@@ -20,6 +20,7 @@ mobile-nixos.kernel-builder {
     ./0001-HACK-don-t-reflect-EBC-output.patch
     ./0001-mfd-rk808-Fix-power-key-polarity.patch
     ./0001-HACK-Disable-clm_blob-loading-in-brcmfmac.patch
+    ./0001-WIP-pinenote-Declare-panel-orientation.patch
   ];
 
   postInstall = ''
