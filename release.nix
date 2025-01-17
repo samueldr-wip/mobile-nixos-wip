cbc5eb9de2eca7672f86a6e950736069d1c1d9df
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-17 14:57:21 -0500

    WIP

--- release.nix
+++ release.nix
@@ -1,6 +1,3 @@
-let
-  pp = x: builtins.trace x x;
-in
 # What's release.nix?
 # ===================
 #
@@ -79,26 +76,19 @@ let
     else mobileReleaseTools.all-devices
   ;
 
-  inherit (pkgs)
-    lib
-  ;
-
-  inherit (lib)
+  inherit (pkgs.lib)
     filterAttrs
     filterAttrsRecursive
     genAttrs
     getAttrFromPath
-    isAttrs
     isDerivation
     isList
-    last
     mapAttrsRecursive
     optionalAttrs
+    unique
   ;
 
   inherit (mobileReleaseTools)
-    makeReleaseJob
-    makeSkippedOverlayJob
     readOverlayAttributeNames
     recurseIntoPackageSet
   ;
@@ -106,13 +96,12 @@ let
   inherit (mobileReleaseTools.withPkgs pkgs)
     evalFor
     evalWithConfiguration
-    knownSystems
     specialConfig
   ;
 
   # Systems we should eval for, per host system.
   # Non-native will be assumed cross.
-  crossTargetsFromSystem =
+  fromLocalSystemToCrossTargets =
     {
       x86_64-linux = [
         "armv7l-linux"
@@ -126,6 +115,28 @@ let
         "armv7l-linux"
       ];
     }
+  ;
+
+  # For a given system, return systems on which it could be compiled from.
+  # In other words, given a system in this attrset, list all systems it should be evaluated on.
+  fromTargetToSystems =
+    builtins.listToAttrs
+    (
+      builtins.map
+      (
+        system:
+
+        {
+          name = system;
+          value =
+            builtins.filter
+            (name: (builtins.elem system fromLocalSystemToCrossTargets.${name}))
+            (builtins.attrNames fromLocalSystemToCrossTargets)
+          ;
+        }
+      )
+      (unique (builtins.concatLists (builtins.attrValues fromLocalSystemToCrossTargets)))
+    )
   ;
 
   # An attrset of `$device = $system;` entries.
@@ -153,6 +164,14 @@ let
         ];
       };
     };
+    "phosh" = {
+      configuration = {
+        imports = [
+          ./examples/phosh/configuration.nix
+        ];
+      };
+      evalForCross = false;
+    };
     "plasma-mobile" = {
       configuration = {
         imports = [
@@ -164,7 +183,7 @@ let
   };
 
   evalAllConfigs =
-    { device, system, dryRun }:
+    { device, system, dryRun, releaseConfigs }:
     genAttrs (builtins.attrNames releaseConfigs)
     (
       name:
@@ -172,7 +191,6 @@ let
         {
           inherit name;
           configuration = {};
-          evalForCross = true;
         } // releaseConfigs.${name}
       );
       let
@@ -182,6 +200,7 @@ let
       then "<unrealized eval for ${device} ${name}>"
       else {
         inherit (eval.config.mobile.outputs) default initrd toplevel;
+        kernel = eval.config.mobile.boot.stage-1.kernel.package;
       }
     )
   ;
@@ -189,13 +208,16 @@ let
   evalDeviceForSystem =
     { system, device, dryRun }:
 
+    let
+      crossReleaseConfigs = filterAttrs (name: value: value.evalForCross or true) releaseConfigs;
+    in
     {
-      cross = genAttrs (builtins.filter (el: el != system) systems) ( # XXX wrong list; I need a reverse lookup. i.e. for an aarch64-linux device, cross from x86_64; for an x86_64-linux device, no cross
+      cross = genAttrs (builtins.filter (el: el != system) (fromTargetToSystems.${system})) (
         localSystem:
-        evalAllConfigs { inherit device dryRun; system = localSystem; }
+        evalAllConfigs { inherit device dryRun; system = localSystem; releaseConfigs = crossReleaseConfigs; }
       );
     } // (optionalAttrs (builtins.elem system systems) {
-      native = evalAllConfigs { inherit device system dryRun; };
+      native = evalAllConfigs { inherit device system dryRun releaseConfigs; };
     })
   ;
 
@@ -266,12 +288,12 @@ let
                     buildingForSystem = name;
                   });
                 }
-              ) crossTargetsFromSystem.${system})
+              ) fromLocalSystemToCrossTargets.${system})
           ;
           crossSystems =
             builtins.filter
             (el: el != system)
-            crossTargetsFromSystem.${system}
+            fromLocalSystemToCrossTargets.${system}
           ;
         in
         ({
@@ -293,9 +315,12 @@ let
   # and additional metadata / configuration that the CI infrastructure can use.
   CI = {
     _data = {
-      inherit deviceSystems;
-      # XXX meeeeeeeeeeeeh... not useful since there's no AArch64 runners ffs.
-      # though maybe using qemu binfmt emulation 
+      inherit
+        fromLocalSystemToCrossTargets
+        fromTargetToSystems
+        deviceSystems
+      ;
+# XXX we now have access to arm64 runners :o
 ##### XXX #####      buildInCI = [
 ##### XXX #####        # This list of paths is used by the CI over on github to create
 ##### XXX #####        # a matrix of packages to build.
@@ -329,320 +354,3 @@ in
 if evalForCI
 then CI
 else CI.jobs
-
-###   kernelJobs =
-###     builtins.concatLists
-###     (
-###       builtins.map
-###       (device:
-###         builtins.map
-###         (
-###           system:
-###           let
-###             eval =
-###               evalWithConfiguration {
-###                 nixpkgs.localSystem = knownSystems.${system};
-###               } device
-###             ;
-###             kernel = eval.config.mobile.boot.stage-1.kernel.package;
-###           in
-###           makeReleaseJob {
-###             path =
-###               if eval.config.nixpkgs.crossSystem == null
-###               then "kernel.native.${device}"
-###               else "cross.from_${system}.${device}.kernel"
-###             ;
-###             value = kernel;
-###           }
-###         )
-###         systems
-###       )
-###       devices
-###     )
-###   ;
-### 
-###   installerJobs = 
-###   let
-###     evalInstaller =
-###       { device
-###       , localSystem
-###       }:
-###       let
-###         eval = evalWithConfiguration {
-###           imports = [
-###             ./examples/installer/configuration.nix
-###           ];
-###           nixpkgs.localSystem = knownSystems.${localSystem};
-###         } device;
-###       in
-###       eval // { inherit (eval.config.mobile) outputs; }
-###     ;
-###   in
-###     builtins.concatLists
-###     (
-###       builtins.map
-###       (device:
-###         builtins.map
-###         (
-###           localSystem:
-###           let
-###             eval =
-###               evalInstaller {
-###                 inherit device;
-###                 localSystem = localSystem;
-###               }
-###             ;
-###             installer = eval.outputs.default;
-###           in
-###           makeReleaseJob {
-###             path =
-###               if eval.config.nixpkgs.crossSystem == null
-###               then "installer.${device}"
-###               else "cross.from_${localSystem}.${device}.installer"
-###               ;
-###             value = installer;
-###           }
-###         )
-###         systems
-###       )
-###       devices
-###     )
-###   ;
-### 
-###   # TODO: evaluate example systems [cross and native] [only hello]
-###   exampleJobs = 
-###     []
-###   ;
-### 
-###   jobset = 
-###     [
-###       (makeReleaseJob { path = "documentation"; value =
-###         import ./doc {
-###           inherit pkgs;
-###         };
-###       })
-###       (makeReleaseJob { path = "shell"; value =
-###         import ./shell.nix {
-###           inherit pkgs;
-###         };
-###       })
-###     ]
-###     ++ overlayJobs
-###     ++ kernelJobs
-###     ++ installerJobs
-###     ++ exampleJobs
-###   ;
-### 
-### in
-### 
-### if evalForCI
-### then CI
-### else CI.jobs
-### 
-### ### # This weird shuffle is to make the `device` argument depend on the input `pkgs`,
-### ### # while also keeping the original `devices` argument name in the code..
-### ### let devices' = devices; in
-### ### let
-### ### in
-### ### # Drop this unneeded name.
-### ### let devices' = null; in
-### ### 
-### ### let
-### ###   # We require some `lib` stuff in here.
-### ###   # Pick a lib from the arbitrary package set.
-### ###   inherit (pkgs) lib releaseTools;
-### ###   inherit (mobileReleaseTools.withPkgs pkgs)
-### ###     evalFor
-### ###     evalWithConfiguration
-### ###     knownSystems
-### ###     specialConfig
-### ###   ;
-### ###   # `device` here is indexed by the system it's being built on first.
-### ###   # FIXME: can we better filter this?
-### ###   device = lib.genAttrs devices (device:
-### ###     lib.genAttrs systems (system:
-### ###       (evalWithConfiguration {
-### ###         nixpkgs.localSystem = knownSystems.${system};
-### ###       } device).config.mobile.outputs.default
-### ###     )
-### ###   );
-### ### 
-### ###   evalExample =
-### ###     { example
-### ###     , system
-### ###     , targetSystem ? system
-### ###     }:
-### ###     import example {
-### ###       inherit pkgs;
-### ###       device = specialConfig {
-### ###         name =
-### ###           if system == targetSystem
-### ###           then system
-### ###           else "${targetSystem}-built-on-${system}"
-### ###         ;
-### ###         inherit system;
-### ###         buildingForSystem = targetSystem;
-### ###         config = {
-### ###           # Ensures outputs are digestible by Hydra
-### ###           mobile._internal.compressLargeArtifacts = inNixOSHydra;
-### ###           # Build a generic rootfs
-### ###           mobile.rootfs.shared.enabled = true;
-### ###         };
-### ###       };
-### ###     }
-### ###   ;
-### ### 
-### ###   evalInstaller =
-### ###     { device
-### ###     , localSystem
-### ###     }:
-### ###     let
-### ###       eval = evalWithConfiguration {
-### ###         imports = [
-### ###           ./examples/installer/configuration.nix
-### ###         ];
-### ###         nixpkgs.localSystem = knownSystems.${localSystem};
-### ###       } device;
-### ###     in
-### ###       eval // { inherit (eval.config.mobile) outputs; }
-### ###   ;
-### 
-### ###   # The main attributes of the release.
-### ###   release = rec {
-### ###   inherit device;
-### ###   inherit kernel;
-### ### 
-### ###   # Some example systems to build.
-### ###   # They track breaking changes, and ensures dependencies are built.
-### ###   # They may or may not work as-they-are on devices. YMMV.
-### ###   examples = {
-### ###     hello = {
-### ###       x86_64-linux.toplevel  = (evalExample { example = ./examples/hello; system = "x86_64-linux"; }).outputs.toplevel;
-### ###       aarch64-linux.toplevel = (evalExample { example = ./examples/hello; system = "aarch64-linux"; }).outputs.toplevel;
-### ###       cross-x86-aarch64.toplevel = (evalExample { example = ./examples/hello; system = "x86_64-linux"; targetSystem = "aarch64-linux"; }).outputs.toplevel;
-### ###       cross-x86-armv7l.toplevel  = (evalExample { example = ./examples/hello; system = "x86_64-linux"; targetSystem = "armv7l-linux";  }).outputs.toplevel;
-### ###     };
-### ###     phosh = {
-### ###       x86_64-linux.toplevel  = (evalExample { example = ./examples/phosh; system = "x86_64-linux"; }).outputs.toplevel;
-### ###       aarch64-linux.toplevel = (evalExample { example = ./examples/phosh; system = "aarch64-linux"; }).outputs.toplevel;
-### ###       # Disabled: uses large amount of memory and OOMs
-### ###       #cross-x86-aarch64.toplevel = (evalExample { example = ./examples/phosh; system = "x86_64-linux"; targetSystem = "aarch64-linux"; }).outputs.toplevel;
-### ###     };
-### ###     plasma-mobile = {
-### ###       x86_64-linux.toplevel  = (evalExample { example = ./examples/plasma-mobile; system = "x86_64-linux"; }).outputs.toplevel;
-### ###       aarch64-linux.toplevel = (evalExample { example = ./examples/plasma-mobile; system = "aarch64-linux"; }).outputs.toplevel;
-### ###       # Disabled: uses large amount of memory and OOMs
-### ###       #cross-x86-aarch64.toplevel = (evalExample { example = ./examples/plasma-mobile; system = "x86_64-linux"; targetSystem = "aarch64-linux"; }).outputs.toplevel;
-### ###     };
-### ###   };
-### ### 
-### ###   installer = {
-### ###     lenovo-krane = (evalInstaller { device = "lenovo-krane"; localSystem = "aarch64-linux"; }).outputs.default;
-### ###     lenovo-wormdingler = (evalInstaller { device = "lenovo-wormdingler"; localSystem = "aarch64-linux"; }).outputs.default;
-### ###     pine64-pinephone = (evalInstaller { device = "pine64-pinephone"; localSystem = "aarch64-linux"; }).outputs.default;
-### ###     pine64-pinephonepro = (evalInstaller { device = "pine64-pinephonepro"; localSystem = "aarch64-linux"; }).outputs.default;
-### ###   };
-### ### 
-### ###   cross-compiled = {
-### ###     installer = {
-### ###       lenovo-krane = (evalInstaller { device = "lenovo-krane"; localSystem = "x86_64-linux"; }).outputs.default;
-### ###       lenovo-wormdingler = (evalInstaller { device = "lenovo-wormdingler"; localSystem = "x86_64-linux"; }).outputs.default;
-### ###       pine64-pinephone = (evalInstaller { device = "pine64-pinephone"; localSystem = "x86_64-linux"; }).outputs.default;
-### ###       pine64-pinephonepro = (evalInstaller { device = "pine64-pinephonepro"; localSystem = "aarch64-linux"; }).outputs.default;
-### ###     };
-### ###   };
-### ###   };
-### 
-### 
-### 
-### 
-### 
-### 
-### 
-### 
-### ###   # When evaluating a "full" release, on a bigger system (needs a lot of RAM)
-### ###   fullReleaseContents = with release; {
-### ###   tested = let
-### ###     hasSystem = name: lib.lists.any (el: el == name) systems;
-### ### 
-### ###     constituents =
-### ###       cross-canaries.aarch64-linux.constituents
-### ###       ++ lib.optionals (hasSystem "x86_64-linux") [
-### ###         device.uefi-x86_64.x86_64-linux              # UEFI system
-### ### 
-### ###         # Cross builds
-### ###         device.motorola-potter.x86_64-linux          # Android
-### ###         device.asus-dumo.x86_64-linux                # Depthcharge
-### ### 
-### ###         # Example systems
-### ###         examples.hello.x86_64-linux.toplevel
-### ###         examples.hello.cross-x86-aarch64.toplevel
-### ###         examples.phosh.x86_64-linux.toplevel
-### ###         examples.plasma-mobile.x86_64-linux.toplevel
-### ### 
-### ###         # Flashable zip binaries are universal for a platform.
-### ###         overlay.x86_64-linux.aarch64-linux-cross.mobile-nixos.android-flashable-zip-binaries
-### ###       ]
-### ###       ++ lib.optionals (hasSystem "aarch64-linux") [
-### ###         device.motorola-potter.aarch64-linux         # Android
-### ###         device.asus-dumo.aarch64-linux               # Depthcharge
-### ### 
-### ###         # Example systems
-### ###         examples.hello.aarch64-linux.toplevel
-### ###         examples.phosh.aarch64-linux.toplevel
-### ###         examples.plasma-mobile.aarch64-linux.toplevel
-### ### 
-### ###         installer.pine64-pinephone
-### ### 
-### ###         # Flashable zip binaries are universal for a platform.
-### ###         overlay.aarch64-linux.aarch64-linux.mobile-nixos.android-flashable-zip-binaries
-### ###       ];
-### ###   in
-### ###   releaseTools.aggregate {
-### ###     name = "mobile-nixos-tested";
-### ###     inherit constituents;
-### ###     meta = {
-### ###       description = "Representative subset of devices that have to succeed.";
-### ###     };
-### ###   };
-### ### 
-### ###   # Uses the constituents of tested
-### ###   testedPlus = let
-### ###     hasSystem = name: lib.lists.any (el: el == name) systems;
-### ### 
-### ###     constituents = tested.constituents
-### ###       ++ cross-canaries.armv7l-linux.constituents
-### ###       ++ lib.optionals (hasSystem "x86_64-linux") [
-### ###         # FIXME: add an armv7l system once one is available again
-### ###         # device.asus-flo.x86_64-linux
-### ###         overlay.x86_64-linux.armv7l-linux-cross.mobile-nixos.android-flashable-zip-binaries
-### ###         examples.hello.cross-x86-armv7l.toplevel
-### ###       ]
-### ###       ++ lib.optionals (hasSystem "aarch64-linux") [
-### ###       ]
-### ###       ++ lib.optionals (hasSystem "armv7l-linux") [
-### ###         # FIXME: add an armv7l system once one is available again
-### ###         # device.asus-flo.armv7l-linux
-### ###         overlay.armv7l-linux.armv7l-linux.mobile-nixos.android-flashable-zip-binaries
-### ###       ]
-### ###       ;
-### ###   in
-### ###   releaseTools.aggregate {
-### ###     name = "mobile-nixos-tested-plus";
-### ###     inherit constituents;
-### ###     meta = {
-### ###       description = ''
-### ###         Other targets that may be failing more often than `tested`.
-### ###         This contains more esoteric and less tested platforms.
-### ### 
-### ###         For a future release, `testedPlus` shoud also pass.
-### ###       '';
-### ###     };
-### ###   };
-### ###   };
-### ### in
-### ### release // (
-### ###   if fullRelease
-### ###   then fullReleaseContents
-### ###   else {}
-### ### )
